78b6de310ac8c14d2e9b595acc40d9bcd7ddad2a
Paul Lovette <lightiv@gmail.com>
2021-04-02 19:56:49 +0000

    Updated setup.sh which allows for creating Althea keys.

--- files/setup.sh
+++ files/setup.sh
@@ -1,5 +1,5 @@
 printf "############################################################################################# \n"
-printf "#     Script for installing the Akash node by Paul D. Lovette | 02-02-2021                  # \n"
+printf "#     Script for installing the Althea node by Paul D. Lovette | 04-02-2021                 # \n"
 printf "#     Find me at https://skynet.paullovette.com/ or E-Mail: skynet@paullovette.com          # \n"
 printf "#     While I have tested this script and it works great on my Ubuntu 20.0.4 install        # \n"
 printf "#                                                                                           # \n"
