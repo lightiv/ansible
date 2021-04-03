df1e05639c53addf0f8aa6ab3296bd6982b7af79
Paul Lovette <lightiv@gmail.com>
2021-04-03 06:17:10 +0000

    Updated setup.sh

--- files/setup.sh
+++ files/setup.sh
@@ -21,3 +21,45 @@ printf "$(date) *** STOP *** Copy the displayed key above! *** \n";
 
 printf "\n";
 printf "$(date) *** STOP *** Copy the displayed mnemonic above! *** \n";
+
+printf "\n";
+printf "$(date) *** PAUSING 30 SECONDS *** Copy/Write down the displayed ADRESS & MNEMONIC above! *** \n";
+
+printf "\n";
+printf "$(date) Setting up the Geth Ethereum Light Client \n";
+
+cd ~/
+tar -xvf geth-linux-amd64-1.10.1-c2d2f4ed.tar.gz
+
+chmod +x /home/$USER/geth-linux-amd64-1.10.1-c2d2f4ed/geth
+sudo cp /home/$USER/geth-linux-amd64-1.10.1-c2d2f4ed/geth /usr/bin/geth
+
+printf "\n";
+printf "$(date) Finalizing Gravity Bridge Services \n";
+
+sed -i "s/^User=.*/"User=$USER"/" /home/$USER/althea-bin/gravity-bridge.service
+sed -i "s/^User=.*/"User=$USER"/" /home/$USER/althea-bin/geth.service
+
+sudo cp /home/$USER/althea-bin/gravity-bridge.service /lib/systemd/system/gravity-bridge.service
+sudo cp /home/$USER/althea-bin/geth.service /lib/systemd/system/geth.service
+
+printf "\n";
+printf "$(date) Enable and start the Gravity Bridge and Geth system services: \n";
+
+sudo systemctl enable gravity-bridge.service
+sudo systemctl start gravity-bridge.service
+
+sudo systemctl enable geth.service
+sudo systemctl start geth.service
+
+printf "\n";
+printf "$(date) *** Allow your node to completely sync befor proceeding with Enable and start the Gravity Bridge and Geth system services: *** \n";
+
+## End this script here and continue with the next one (Step) ##
+## Create you validator before moving to the following. Put these in the next script: ##
+
+read -sp 'Enter your MNEMONIC from above: ' MNEMONIC
+sed -i "s/^    --cosmos-phrase=.*/    --cosmos-phrase=\"$MNEMONIC\" "\\"\\"" /" /home/$USER/althea-bin/orchestratord.service
+
+read -sp 'Enter your Ethereum Delage Address from above: ' ETH_DELEGATE_ADDRESS
+sed -i "s/^    --cosmos-phrase=.*/    --cosmos-phrase=\"$MNEMONIC\" "\\"\\"" /" /home/$USER/althea-bin/orchestratord.service
